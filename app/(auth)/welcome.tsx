e959f54dedc527556c0e189818fa14200466aac5
KarunaShrestha02 <karuna100502@gmail.com>
2024-10-21 17:10:05 +0545

    no message

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -2,7 +2,11 @@ import { Text } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { router } from "expo-router";
+import Swiper from "react-native-swiper";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+import { useRef } from "react";
 const OnBoarding= () => {
+    const swiperRef= useRef<Swiper>(null);
     return(
         <SafeAreaView className="flex h-full items-center justfy-between bg-white">
             <TouchableOpacity 
@@ -14,9 +18,13 @@ const OnBoarding= () => {
             >
                <Text className="text-black text-md font-JakartaBold">Skip</Text> 
             </TouchableOpacity>
+            <Swiper  ref={swiperRef}
+            loop={false}>
+dot={<View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0]"> </View>}
+            </Swiper>
             </SafeAreaView>
         
-        </SafeAreaView>
+       
     );
 
 
