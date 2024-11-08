62c598fa74830b1ecc1e6e1302a937fbd1c375ad
KarunaShrestha02 <karuna100502@gmail.com>
2024-11-08 11:45:03 +0545

    updated types

--- types/type.d.ts
+++ types/type.d.ts
@@ -1,53 +1,139 @@
-import { Stripe } from "stripe";
+import { TextInputProps, TouchableOpacityProps } from "react-native";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+declare interface Driver {
+  id: number;
+  first_name: string;
+  last_name: string;
+  profile_image_url: string;
+  car_image_url: string;
+  car_seats: number;
+  rating: number;
+}
 
-export async function POST(request: Request) {
-  const body = await request.json();
-  const { name, email, amount } = body;
+declare interface MarkerData {
+  latitude: number;
+  longitude: number;
+  id: number;
+  title: string;
+  profile_image_url: string;
+  car_image_url: string;
+  car_seats: number;
+  rating: number;
+  first_name: string;
+  last_name: string;
+  time?: number;
+  price?: string;
+}
 
-  if (!name || !email || !amount) {
-    return new Response(JSON.stringify({ error: "Missing required fields" }), {
-      status: 400,
-    });
-  }
+declare interface MapProps {
+  destinationLatitude?: number;
+  destinationLongitude?: number;
+  onDriverTimesCalculated?: (driversWithTimes: MarkerData[]) => void;
+  selectedDriver?: number | null;
+  onMapReady?: () => void;
+}
 
-  let customer;
-  const doesCustomerExist = await stripe.customers.list({
-    email,
-  });
+declare interface Ride {
+  origin_address: string;
+  destination_address: string;
+  origin_latitude: number;
+  origin_longitude: number;
+  destination_latitude: number;
+  destination_longitude: number;
+  ride_time: number;
+  fare_price: number;
+  payment_status: string;
+  driver_id: number;
+  user_id: string;
+  created_at: string;
+  driver: {
+    first_name: string;
+    last_name: string;
+    car_seats: number;
+  };
+}
 
-  if (doesCustomerExist.data.length > 0) {
-    customer = doesCustomerExist.data[0];
-  } else {
-    const newCustomer = await stripe.customers.create({
-      name,
-      email,
-    });
+declare interface ButtonProps extends TouchableOpacityProps {
+  title: string;
+  bgVariant?: "primary" | "secondary" | "danger" | "outline" | "success";
+  textVariant?: "primary" | "default" | "secondary" | "danger" | "success";
+  IconLeft?: React.ComponentType<any>;
+  IconRight?: React.ComponentType<any>;
+  className?: string;
+}
 
-    customer = newCustomer;
-  }
+declare interface GoogleInputProps {
+  icon?: string;
+  initialLocation?: string;
+  containerStyle?: string;
+  textInputBackgroundColor?: string;
+  handlePress: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => void;
+}
 
-  const ephemeralKey = await stripe.ephemeralKeys.create(
-    { customer: customer.id },
-    { apiVersion: "2024-06-20" },
-  );
+declare interface InputFieldProps extends TextInputProps {
+  label: string;
+  icon?: any;
+  secureTextEntry?: boolean;
+  labelStyle?: string;
+  containerStyle?: string;
+  inputStyle?: string;
+  iconStyle?: string;
+  className?: string;
+}
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: parseInt(amount) * 100,
-    currency: "usd",
-    customer: customer.id,
-    automatic_payment_methods: {
-      enabled: true,
-      allow_redirects: "never",
-    },
-  });
+declare interface PaymentProps {
+  fullName: string;
+  email: string;
+  amount: string;
+  driverId: number;
+  rideTime: number;
+}
 
-  return new Response(
-    JSON.stringify({
-      paymentIntent: paymentIntent,
-      ephemeralKey: ephemeralKey,
-      customer: customer.id,
-    }),
-  );
+declare interface LocationStore {
+  userLatitude: number | null;
+  userLongitude: number | null;
+  userAddress: string | null;
+  destinationLatitude: number | null;
+  destinationLongitude: number | null;
+  destinationAddress: string | null;
+  setUserLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => void;
+  setDestinationLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => void;
+}
+
+declare interface DriverStore {
+  drivers: MarkerData[];
+  selectedDriver: number | null;
+  setSelectedDriver: (driverId: number) => void;
+  setDrivers: (drivers: MarkerData[]) => void;
+  clearSelectedDriver: () => void;
+}
+
+declare interface DriverCardProps {
+  item: MarkerData;
+  selected: number;
+  setSelected: () => void;
 }
